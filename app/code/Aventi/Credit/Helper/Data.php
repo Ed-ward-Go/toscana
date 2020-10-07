642da8bde0dabcf770ee63f358a6d57b16be3a52
emerz <emerz@aventi.com.co>
2020-10-07 09:23:54 -0500

    Validate credit

--- app/code/Aventi/Credit/Helper/Data.php
+++ app/code/Aventi/Credit/Helper/Data.php
@@ -3,31 +3,53 @@
 namespace Aventi\Credit\Helper;
 
 use Aventi\Credit\Model\CreditRepository;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
+    /**
+     * @var CreditRepository
+     */
     private $creditRepository;
+    /**
+     * @var PriceCurrencyInterface
+     */
+    private $priceCurrency;
 
     public function __construct(
-        CreditRepository $creditRepository
+        CreditRepository $creditRepository,
+        PriceCurrencyInterface $priceCurrency
     ) {
         $this->creditRepository = $creditRepository;
+        $this->priceCurrency = $priceCurrency;
     }
 
     public function getTotalList($customerId)
     {
-        $credit = $this->creditRepository->getByCustomerId($customerId);
-
         $data = [];
+        try {
+            $credit = $this->creditRepository->getByCustomerId($customerId);
 
-        if ($credit) {
-            $data = [
-                'Available' => $credit->getAvailable(),
-                'Credit' => $credit->getCredit(),
-                'Balance' => $credit->getBalance()
-            ];
+            if ($credit) {
+                $data = [
+                    'Available' => $this->getFormatedPrice($credit->getAvailable()),
+                    'Credit' => $this->getFormatedPrice($credit->getCredit()),
+                    'Balance' => $this->getFormatedPrice($credit->getBalance())
+                ];
+            }
+        }catch (\Exception $e){
+            return false;
         }
 
         return $data;
+    }
+
+    /**
+     * @param $amount
+     * @return string
+     */
+    public function getFormatedPrice($amount)
+    {
+        return $this->priceCurrency->convertAndFormat($amount);
     }
 }
