8c10a15ede7315215267c3fd3a2de92ef568b142
emerz <emerz@aventi.com.co>
2021-02-15 16:30:48 -0500

    Add condition to send email cancel

--- app/code/Aventi/SAP/Observer/Order/OrderCancelAfter.php
+++ app/code/Aventi/SAP/Observer/Order/OrderCancelAfter.php
@@ -1,20 +1,20 @@
 <?php
 /**
  * Add file comment to orders
- * Copyright (C) 2018  
- * 
+ * Copyright (C) 2018
+ *
  * This file is part of Aventi/OrderComment.
- * 
+ *
  * Aventi/OrderComment is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -59,16 +59,20 @@ class OrderCancelAfter implements \Magento\Framework\Event\ObserverInterface
         $order = $observer->getEvent()->getOrder();
 
         if($order){
-            $this->dataEmail->sendOrderCancelEmail(
-                $order->getCustomerEmail(),                                
-                $order->getCustomerFirstname().' '.$order->getCustomerLastname(),
-                $order->getIncrementId(),
-                $order->getPayment()->getMethodInstance()->getTitle(),
-                $order
-            );
-            $order->setData('sap_notification_send',1);
-            $this->orderRepository->save($order);
-        }        
-        
+            try {
+                $this->dataEmail->sendOrderCancelEmail(
+                    $order->getCustomerEmail(),
+                    $order->getCustomerFirstname() . ' ' . $order->getCustomerLastname(),
+                    $order->getIncrementId(),
+                    $order->getPayment()->getMethodInstance()->getTitle(),
+                    $order
+                );
+                $order->setData('sap_notification_send', 1);
+                $this->orderRepository->save($order);
+            } catch (LocalizedException $exception) {
+                $this->logger->error("The cancel order don't send email: " . $exception->getMessage());
+            }
+        }
+
     }
 }
